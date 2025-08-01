b84b7b19d1a7cdc226c83b809b6f856cf9f28b19
Hyp2k4 <hiephoang1752004@gmail.com>
2025-08-01 23:31:03 +0700

    2nd commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import { connect } from 'mongoose';
-import connectDB from './config/db.js ';
+import connectDB from './config/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controllers/clerkWebhooks.js';
 
