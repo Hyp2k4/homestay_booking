d7c96acd3a89922c7cc0ffc0857c6aba82c4ae8e
Hyp2k4 <hiephoang1752004@gmail.com>
2025-08-14 22:36:50 +0700

    push deploy

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -195,7 +195,7 @@ const RoomDetails = () => {
 
             <div className='flex flex-col items-start gap-4 pt-10'>
                 <div className='flex gap-4'>
-                    <img src={room.homestay?.owner.image} alt="Host" className='h-14 w-14 md:h-18 md:w-18 rounded-full' />
+                    <img src={room.homestay?.owner?.image} alt="Host" className='h-14 w-14 md:h-18 md:w-18 rounded-full' />
                     <div>
                         <p className='text-lg md:text-xl'>Hosted by: {room.homestay?.name}</p>
                         <div className='flex items-center mt-1 '>
